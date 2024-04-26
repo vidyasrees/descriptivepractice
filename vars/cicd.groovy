72f59d5274698cc3c3fe6097eff38442ec5928e7
vidyasrees <119058981+vidyasrees@users.noreply.github.com>
2024-04-26 20:43:00 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,7 @@ def newDeploy(workspace,ip,context)
 {
     sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+def runSelenium(workspace)
+{
+    sh "java -jar /var/lib/jenkins/workspace/dpwithmath/testing.jar"
+}
